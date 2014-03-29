9054ad12bcca272e98320c3f702ceedbfca6e05a
jfols <email@justinfoley.me>
2014-03-29 02:33:34 +0000

    meteor update v0.8.0

--- client/views/layout/header.js
+++ client/views/layout/header.js
@@ -4,6 +4,8 @@ Template.header.helpers({
     var args = Array.prototype.slice.call(arguments, 0);
     // remove the hash added by at the end by Handlebars
     args.pop();
+    // temporary hack for 0.8.0
+    return false;
     var active = _.any(args, function(name) {
       return Router.current().route.name === name;
     });
